69468a07fc2ebc829e9d01ddb91400b9fc4a82bb
primaustryn <bingo13@snu.ac.kr>
2023-12-18 13:14:52 +0900

    submitted 6294

---
+++ 6294_avg_score/submission.py
@@ -0,0 +1,27 @@
+import sys
+
+#1 N, K
+N,K = list(map(int, (input()).split(' ')))
+
+#2 S1~Sn
+scores = list(map(int, (input()).split(' ')))
+
+#3 Ai, Bi for i = 0~K
+listA=[]
+listB=[]
+for i in range(0,K):
+  A,B=list(map(int, input().split(' ')))
+  listA.append(A)
+  listB.append(B)
+
+for i in range(0,K):
+  score = 0
+  if(listA[i] == listB[i]):
+    print(scores[listA[i]])
+  for j in range(listA[i]-1, listB[i]):
+    score += scores[j]
+  print(format(score/(listB[i]-listA[i]+1),".2f"))
+  
+    
+
+  
