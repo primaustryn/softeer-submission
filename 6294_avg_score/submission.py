bfe80fa13e181c95bc85350e228e7a97a41fa664
primaustryn <bingo13@snu.ac.kr>
2023-12-18 13:17:51 +0900

    resolve corner case

--- 6294_avg_score/submission.py
+++ 6294_avg_score/submission.py
@@ -16,8 +16,6 @@ for i in range(0,K):
 
 for i in range(0,K):
   score = 0
-  if(listA[i] == listB[i]):
-    print(scores[listA[i]])
   for j in range(listA[i]-1, listB[i]):
     score += scores[j]
   print(format(score/(listB[i]-listA[i]+1),".2f"))
